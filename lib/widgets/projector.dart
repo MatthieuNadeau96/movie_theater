a8d80b229373625fd12e493b9a529875ba547be8
Matthieu <matthieunadeau96@gmail.com>
2020-08-01 15:58:42 -0400

    Built the screen projection with Paint for the SeatScreen

--- lib/widgets/projector.dart
+++ lib/widgets/projector.dart
@@ -1,32 +1,67 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 
 class Projector extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      painter: ScreenPainter(),
+      painter: ScreenPainter(
+        color1: Theme.of(context).canvasColor,
+        color2: Theme.of(context).primaryColor,
+      ),
     );
   }
 }
 
 class ScreenPainter extends CustomPainter {
+  final Color color1;
+  final Color color2;
+  ScreenPainter({
+    this.color1,
+    this.color2,
+  });
+
   @override
   void paint(Canvas canvas, Size size) {
     final height = size.height;
     final width = size.width;
-    Paint paint = Paint();
 
+    // Movie Screen
+    Paint screenPaint = Paint();
     Path screenPath = Path();
-    paint.style = PaintingStyle.stroke;
-    paint.strokeWidth = 3;
-
+    screenPaint.style = PaintingStyle.stroke;
+    screenPaint.strokeWidth = 3;
     screenPath.moveTo(0, height * 1);
 
     screenPath.cubicTo(width * 0.25, height * 0.25, width * 0.75, height * 0.25,
         width, height);
 
-    paint.color = Colors.white;
-    canvas.drawPath(screenPath, paint);
+    screenPaint.color = Colors.white;
+    canvas.drawPath(screenPath, screenPaint);
+
+    // Movie Projection
+    Path projection = Path();
+    Paint projectionPaint = Paint()
+      ..shader = ui.Gradient.linear(
+        Offset(50, 0),
+        Offset(50, 60),
+        [
+          color1,
+          color2.withOpacity(0),
+        ],
+      );
+
+    projection.moveTo(0, height * 1);
+    projection.cubicTo(width * 0.25, height * 0.26, width * 0.75, height * 0.26,
+        width, height);
+    // projection.lineTo(0, height)
+    projection.cubicTo(
+        width * 0.25, height / 0.75, width * 0.75, height / 0.75, 0, height);
+    projection.close();
+
+    projectionPaint.color = Colors.red;
+    canvas.drawPath(projection, projectionPaint);
   }
 
   @override
